c235f3414d4197d6495734fd8233f9d74cd9bcc1
ayoub <ayoubousbia02@gmail.com>
2024-05-06 19:38:58 +0200

    change pointer caracter to '@' and move to 4 directions

--- snake/app.py
+++ snake/app.py
@@ -1,14 +1,44 @@
 import curses
-from curses import wrapper
 
 
 def main(stdscr):
     stdscr.clear()
-    stdscr.addstr(10, 10, "Hello World")
+
+    curses.curs_set(0)
+
+    y, x = 0, 0
+
+    stdscr.addch(y, x, "@")
 
     stdscr.refresh()
 
-    stdscr.getch()
+    while True:
+        key = stdscr.getkey()
+
+        cur_y, cur_x = stdscr.getyx()
+
+        if key == "KEY_UP":
+            y = max(y - 1, 0)
+
+        elif key == "KEY_DOWN":
+            y = min(y + 1, stdscr.getmaxyx()[0] - 1)
+
+        elif key == "KEY_LEFT":
+            x = max(x - 1, 0)
+
+        elif key == "KEY_RIGHT":
+            x = min(x + 1, stdscr.getmaxyx()[1] - 1)
+
+        elif key == "q":
+            break
+
+        stdscr.addch(cur_y, cur_x, " ")
+
+        stdscr.move(y, x)
+
+        stdscr.addch("@")
+
+        stdscr.refresh()
 
 
-wrapper(main)
+curses.wrapper(main)
